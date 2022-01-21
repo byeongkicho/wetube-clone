0d88974378cd2cc05ded28c1daca3ac5075a2b6c
taboo32 <byeongkicho@gmail.com>
2022-01-21 01:38:55 -0600

    04. Middleware
    01.21.2022
    1. Middleware can be handlers and handler cna be middleware.
    2. return will end respond.
    3. next() will calls the next function.
    4. If you want to you middleware globally it has to be on top.
    5. morgan has get, path, status code etc...

--- src/server.js
+++ src/server.js
@@ -1,19 +1,23 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const handleHome = (req, res) => {
-  return res.send("<h1> Hello</h1>");
+const home = (req, res) => {
+  console.log("I will respond.");
+  return res.send("Hello");
 };
 
-const handleLogin = (req, res) => {
-  return res.send("Login here.");
+const login = (req, res) => {
+  return res.send("login");
 };
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} ✅ `);
