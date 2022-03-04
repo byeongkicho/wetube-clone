e8e4ccfda66b5a390f3128258520fb2567d85475
taboo32 <byeongkicho@gmail.com>
2022-03-04 00:46:07 -0600

    10. MongoDB and Moongoose

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -47,9 +47,18 @@ export const postEdit = (req, res) => {
   return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
-  return res.render("upload");
+  return res.render("upload", { pageTitle: "Upload Video" });
 };
 export const postUpload = (req, res) => {
-  // here we will add a video to the videos array.
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
   return res.redirect("/");
 };
